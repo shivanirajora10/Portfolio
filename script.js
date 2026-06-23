a83874e633cb6b3c5eed4e91310787705a0d08f8
shivanirajora10 <shivanirajora37810@gmail.com>
2026-06-23 16:44:34 +0530

    heo

--- script.js
+++ script.js
@@ -1,3 +1,31 @@
-console.log(
-"Portfolio Loaded"
+function toggleMenu(){
+
+document
+.getElementById("sidebar")
+.classList
+.toggle("active");
+
+}
+
+
+
+document
+.querySelectorAll("#sidebar a")
+.forEach(link=>{
+
+link
+.addEventListener(
+"click",
+
+()=>{
+
+document
+.getElementById("sidebar")
+.classList
+.remove("active");
+
+}
+
 );
+
+});
